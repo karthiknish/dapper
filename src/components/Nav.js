b3505b577f088b81cc9cc191b86251ee6eee0ede
Karthik <karthik.nishanth06@gmail.com>
2023-07-26 19:02:02 +0100

    logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Router from "next/router";
 import Search from "./Search";
-
+import Logo from "../assets/logo.png";
 function Nav() {
   return (
     <nav className="flex justify-between items-center py-4 border-b">
-      <h1 className="text-2xl font-bold">Welcome to My E-commerce Site</h1>
+      <img
+        onClick={() => Router.push("/")}
+        width="13%"
+        className="ml-4"
+        src={Logo.src}
+      />
+
       <div className="flex items-center">
         <Search />
         <div className="ml-4 flex items-center space-x-4">
