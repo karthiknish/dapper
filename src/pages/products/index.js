3e755739c5505e3b9781fcd555e8b1fb338caed7
Karthik <karthik.nishanth06@gmail.com>
2023-07-27 14:15:20 +0100

    db issue

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,6 +1,7 @@
 import { getProducts } from "../api/contentful";
 import Link from "next/link";
 import Head from "next/head";
+
 function Index({ products }) {
   return (
     <>
@@ -8,26 +9,24 @@ function Index({ products }) {
         <title>All Products</title>
       </Head>
       <main className="m-4">
-        <h2 className="text-xl font-semibold my-4">All Products</h2>
+        <h2 className="text-2xl font-bold my-4">All Products</h2>
 
-        <div className="grid grid-cols-4 m-3 gap-4">
+        <div className="flex flex-wrap justify-between">
           {products.map((product, index) => (
             <Link
               key={index}
               href={`/${product.fields.category}/${product.sys.id}`}
+              className="border m-2 w-64 flex flex-col items-center p-4 hover:border-green-600 hover:shadow-lg transition duration-300"
             >
-              {console.log(product)}
-              <div className="border p-4">
-                <img
-                  className="w-full h-64 object-cover"
-                  src={product.fields.image}
-                  alt={product.fields.title}
-                />
-                <h3 className="mt-2 font-medium">{product.fields.title}</h3>
-                <p className="text-green-600 font-semibold">
-                  £{product.fields.price}
-                </p>
-              </div>
+              <img
+                className="w-full h-64 object-cover mb-2"
+                src={product.fields.image}
+                alt={product.fields.title}
+              />
+              <h3 className="font-medium text-lg">{product.fields.title}</h3>
+              <p className="text-green-600 font-semibold mt-2">
+                £{product.fields.price}
+              </p>
             </Link>
           ))}
         </div>
@@ -37,6 +36,7 @@ function Index({ products }) {
 }
 
 export default Index;
+
 export async function getStaticProps() {
   const products = await getProducts();
 
