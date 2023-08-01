156295d57cb5211b619e34e78b16f7d92d464dd9
Karthik <karthik.nishanth06@gmail.com>
2023-08-01 01:57:31 +0100

    fewf

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -8,27 +8,30 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, email, password, age, sex } = req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already in use." });
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: "Email already in use." });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = new User({
+        name,
+        email,
+        password: hashedPassword,
+        age,
+        sex,
+      });
+
+      await user.save();
+
+      res.status(201).json({ message: "User registered successfully!" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user
-    const user = new User({
-      name,
-      email,
-      password: hashedPassword,
-      age,
-      sex,
-    });
-
-    await user.save();
-
-    res.status(201).json({ message: "User registered successfully!" });
   } else {
-    res.status(405).end(); // Method not allowed
+    res.status(405).end();
   }
 }
+  
